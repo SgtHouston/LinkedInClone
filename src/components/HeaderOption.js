8c832eb407af66396db074a3e88895ab0956a1ba
Chris Houston <chrisbcoding247@gmail.com>
2021-08-26 23:21:00 -0400

    header finished

---
+++ src/components/HeaderOption.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../componentcss/HeaderOption.css';
+import { Avatar } from "@material-ui/core";
+
+function HeaderOption({ avatar, Icon, title }) {
+    return (
+        <div className="headerOption">
+            {/* If I pass in an icon, render icon */}
+            { Icon &&  <Icon className='headerOption__icon' /> }
+
+            {/* If I pass in an avatar, render avatar */}
+            { avatar && <Avatar className='headerOption__icon' src={avatar} /> }
+
+            {/* Title of Option Passed in via header */}
+            <h3 className='headerOption__title'>{title}</h3>
+        </div>
+    )
+}
+
+export default HeaderOption;
